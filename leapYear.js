95193d5aa4f9a49bde0bbba5c254c2eed29c695b
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2023-07-15 00:03:37 +0300

    Create leapYear.js

---
+++ leapYear.js
@@ -0,0 +1,8 @@
+function leapYear(year) {
+    let num = Number(year);
+    if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
+}
