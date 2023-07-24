802c41ededfd7dd3b76547d3781158903aff6df7
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2023-07-25 00:01:01 +0300

    Create oddOccurences.js

---
+++ oddOccurences.js
@@ -0,0 +1,16 @@
+function oddOccurences(input) {
+    let inputArray = input.split(' ').map(e => e.toLowerCase());
+    let wordsObject = {}
+    for (const word of inputArray) {
+        if (!wordsObject.hasOwnProperty(word)) {
+            wordsObject[word] = 0;
+        }
+        wordsObject[word]++;
+    }
+    let arrOfOddElements = Object.entries(wordsObject).filter(double => double % 2 !== 0).sort((a, b) => b[1] - a[1]);
+    let result = '';
+    for (const array of arrOfOddElements) {
+        result += `${array[0]} `;
+    }
+    console.log(result);
+}
